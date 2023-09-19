554cd00a56bbe869680804bccec616f0325ce087
Guilherme Rainho Geraldo <143048354+guirainho@users.noreply.github.com>
2023-09-19 15:25:19 -0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,3 +1,10 @@
+a) Desenvolva uma função que receba um número inteiro N como parâmetro e
+que imprima na tela a tabuada deste número.
+b) Desenvolva um programa que chame a função desenvolvida para obter as
+tabuadas de 1 a 9.
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #include <stdio.h>
 void tabuada(int N) {
     printf("Tabuada do número %d:\n", N);
