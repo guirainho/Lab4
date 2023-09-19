f3daa991f4c7f31391c492159738b5f4907364e2
Guilherme Rainho Geraldo <143048354+guirainho@users.noreply.github.com>
2023-09-19 15:24:55 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,3 +1,19 @@
+
+a) Desenvolva duas funções:
+• a função c2f, que tem um único parâmetro (o valor da temperatura na escala Celsius)
+e que retorna a temperatura convertida para a escala Fahrenheit;
+• a função f2c, que tem um único parâmetro (o valor da temperatura na escala
+Fahrenheit) e que retorna a temperatura convertida para a escala Celsius;
+b) Implemente um programa que:
+• peça para o usuário fazer uma escolha em um menu com duas opções:
+(1) Converter de Celsius para Fahrenheit
+(2) Converter de Fahrenheit para Celsius
+• peça para o usuário fornecer o valor da temperatura que deve ser convertido;
+• chame a função que faça a conversão solicitada e imprima na tela o valor retornado
+pela função.
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 #include <stdio.h>
 
 float c2f(float celsius){
